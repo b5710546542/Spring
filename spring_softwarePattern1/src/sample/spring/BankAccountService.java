521c7f5c1aca8a8416c68e4b4e893dffc799f8ff
Noot Jettananurak <Noot Jettananurak>
2017-04-19 16:09:58 +0700

    first commit

---
+++ spring_softwarePattern1/src/sample/spring/BankAccountService.java
@@ -0,0 +1,8 @@
+package sample.spring;
+
+/**
+ * Created by Jidapar Jettananurak on 4/10/2017 AD.
+ */
+public interface BankAccountService {
+    int createBankAccount(BankAccountDetails bankAccountDetails);
+}
